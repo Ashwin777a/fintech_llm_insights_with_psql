8beb99ef0ac3e22b7a5b273bfd89e1307900ec3b
Ashwin D <99615439+Ashwin777a@users.noreply.github.com>
2025-09-13 19:59:32 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import requests
+from app import app as fastapi_app
 
 st.title("Daily Fintech Stock Insights")
 
-API_URL = "http://localhost:8000/run-daily-pipeline"
+# --- Run FastAPI in background ---
+def run_api():
+    uvicorn.run(fastapi_app, host="0.0.0.0", port=8000)
+#API_URL = "http://localhost:8000/run-daily-pipeline"
 data = requests.get(API_URL).json()
 
 st.subheader(f"Stock Data ({data['date']})")
